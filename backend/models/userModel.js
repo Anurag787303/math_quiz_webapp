be0c9bcab4a3eff59feab0739076a115d329c2ea
monkey-on-drugs <kesharwanimanas.college@gmail.com>
2024-02-04 16:59:01 +0530

    fixed some changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,70 +1,70 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please enter your name"],
-        maxLength: [30, "Name cannot exceed more than 30 characters"],
-        minLength: [3, "Name should have more than 2 characters"]
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    maxLength: [30, "Name cannot exceed more than 30 characters"],
+    minLength: [3, "Name should have more than 2 characters"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please Enter Your Password"],
+    minLength: [8, "Password should have more than 8 characters"],
+    select: false,
+  },
+  runs: [
+    {
+      answers: {
+        t1: {
+          q1: Boolean,
+          q2: Boolean,
+        },
+        t2: {
+          q1: Boolean,
+          q2: Boolean,
+          q3: Boolean,
+          q4: Boolean,
+        },
+        t3: {
+          q1: Boolean,
+          q2: Boolean,
+          q3: Boolean,
+          q4: Boolean,
+        },
+      },
+      submittedAt: String,
+      time_taken: String,
+      score: Number,
+      createdAt: {
+        type: Date,
+        default: new Date(),
+      },
     },
-    password: {
-        type: String,
-        required: [true, "Please Enter Your Password"],
-        minLength: [8, "Password should have more than 8 characters"],
-        select: false
-    },
-    runs: [
-        {
-            answers: {
-                t1: {
-                    q1: Boolean,
-                    q2: Boolean
-                },
-                t2: {
-                    q1: Boolean,
-                    q2: Boolean,
-                    q3: Boolean,
-                    q4: Boolean,
-                },
-                t3: {
-                    q1: Boolean,
-                    q2: Boolean,
-                    q3: Boolean,
-                    q4: Boolean
-                }
-            },
-            submittedAt: String,
-            time_taken: String,
-            score: Number,
-            createdAt: {
-                type: Date,
-                default: new Date()
-            }
-        }
-    ],
-})
+  ],
+});
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        next()
-    }
+  if (!this.isModified("password")) {
+    next();
+  }
 
-    this.password = await bcrypt.hash(this.password, 10)
-})
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 // JWT Token
 userSchema.methods.getJWTToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE
-    })
-}
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 // Compare Password
 userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-module.exports = mongoose.model("User", userSchema) 
+module.exports = mongoose.model("User", userSchema);
